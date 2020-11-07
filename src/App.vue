572f48fa86f714e8c5b40d4c3a85df82bb30ad39
JinFan1314 <1582516092@qq.com>
2020-11-07 15:59:19 +0800

    文章管理

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,16 @@
 <template>
   <div id="app">
-    <router-view/>
+    <div class="header-part">顶部内容</div>
+    <div class="content-box">
+      <div class="nav">
+        <router-link to="/home">首页</router-link>
+        <br>
+        <router-link to="/article">文章</router-link>
+      </div>
+      <div class="content">
+        <router-view/>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -15,7 +25,19 @@ export default {
 </script>
 
 <style>
-#app {
-
+.header-part{
+  background-color:#98aaff;
 }
+  .nav{
+    background: antiquewhite;
+    width: 159px;
+    height: 500px;
+    float: left;
+  }
+  .content{
+    background: aliceblue;
+    width: 800px;
+    height: 500px;
+    float: left;
+  }
 </style>
--- src/page/content/post-article.vue
+++ src/page/content/post-article.vue
@@ -1,10 +1,12 @@
 <template>
-  <div></div>
+  <div>
+    文章发表
+  </div>
 </template>
 
 <script>
 export default {
-  name: "index-page"
+name: "post-article"
 }
 </script>
 
